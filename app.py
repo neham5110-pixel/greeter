3c45d6d22b6971da40aeb70bbd8786462c669bcd
Neha Muhammad <neham5110@gmail.com>
2026-09-25 16:06:34 -0800

    change greeting word to Hey

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ class GreeterHandler(BaseHTTPRequestHandler):
         self.send_response(200),
         self.send_header("Content-type", "text/html"),
         self.end_headers(),
-        self.wfile.write(b"Hello from Neha!, today is saturday")
+        self.wfile.write(b"Hey from Neha!, today is saturday")
     )
 
 if __name__ == "__main__":
